add34950d6f9fb98c55ad59564a05f36c4541279
Jean Britz <britz.jean@gmail.com>
2024-01-01 07:03:38 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jeanbritz/go-android-bruteforce-pin.git
+
+go 1.21.5
+
+require (
+	github.com/Tryanks/go-accessoryhid v0.0.0-20231214032111-d11ee90149d1 // indirect
+	github.com/google/gousb v1.1.2 // indirect
+)
---
+++ pkg/hid/touchscreen.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/Tryanks/go-accessoryhid"
+)
+
+type Touchscreen struct {
+	Accessory *accessory.Accessory
+}
+
+type Actions interface {
+	SetPosition(x int16, y int16)
+	Press()
+}
+
+func (t *Touchscreen) SetPosition(x int16, y int16) {
+	xMsb := GetMSB(x)
+	xLsb := GetLSB(x)
+	yMsb := GetMSB(y)
+	yLsb := GetLSB(y)
+
+	// Use Pointer to set absolute coordinates
+	err := t.Accessory.SendEvent([]byte{
+		0x02, byte(xLsb), byte(xMsb), byte(yLsb), byte(yMsb),
+	})
+
+	if err != nil {
+		err = fmt.Errorf("error occurred while setting pointer position %w", err)
+		fmt.Println(err)
+	}
+}
+
+func (t *Touchscreen) Press() {
+	// Convert Pointer to Touch Accessory
+	err := t.Accessory.SendEvent([]byte{
+		0x01, 0, 0, 0, 0,
+	})
+
+	if err != nil {
+		err = fmt.Errorf("error occurred while converting %w", err)
+		fmt.Println(err)
+	}
+	// Press
+	err = t.Accessory.SendEvent([]byte{
+		0x00, 0, 0, 0, 0,
+	})
+
+	if err != nil {
+		err = fmt.Errorf("error occurred while trying to press %w", err)
+		fmt.Println(err)
+	}
+}
